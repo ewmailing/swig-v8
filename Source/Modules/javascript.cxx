c16f02c863fc2fb5a86364131e89150ab11e4ce9
Eric Wing <ewing -dot- public |-at-| gmail.com>
2013-09-09 16:44:49 -0700

    Javascript v8: Initial fix for %rename problem documented here: https://github.com/oliver----/swig-v8/issues/18. It looks like more may be needed requiring an audit of the existing code. But this solves the section for variables.
    In exitVariable, if I change the lines that look like:
    .replace(T_NAME, state.variable(NAME))
    to
    .replace(T_NAME, Swig_name_mangle(state.variable(NAME)))
    this fixes the problem.
    
    Using state.variable(NAME_MANGLED) does not work though.
    
    This might suggest there are more places that need Swig_name_mangle() to account for the possibility of %rename being in use.
    
    Oliver responded with:
    Thanks. You are absolutely right... I have to revisit all the places and consider usage of Swig_name_mangle() where it is possible.
    
    This "fix" does not work with JSCore so it is still broken.

--- Source/Modules/javascript.cxx
+++ Source/Modules/javascript.cxx
@@ -2055,7 +2055,7 @@ int V8Emitter::exitVariable(Node* n)
     if(GetFlag(state.variable(), IS_STATIC) || Equal(Getattr(n, "nodeType"), "enumitem") ) {
       Template t_register = getTemplate("jsv8_register_static_variable");
       t_register.replace(T_PARENT, state.clazz(NAME_MANGLED))
-          .replace(T_NAME, state.variable(NAME))
+          .replace(T_NAME, Swig_name_mangle(state.variable(NAME)))
           .replace(T_GETTER, state.variable(GETTER))
           .replace(T_SETTER, state.variable(SETTER))
           .trim()
@@ -2063,7 +2063,7 @@ int V8Emitter::exitVariable(Node* n)
     } else {
       Template t_register = getTemplate("jsv8_register_member_variable");
       t_register.replace(T_NAME_MANGLED, state.clazz(NAME_MANGLED))
-          .replace(T_NAME, state.variable(NAME))
+          .replace(T_NAME, Swig_name_mangle(state.variable(NAME)))
           .replace(T_GETTER, state.variable(GETTER))
           .replace(T_SETTER, state.variable(SETTER))
           .trim()
@@ -2074,7 +2074,7 @@ int V8Emitter::exitVariable(Node* n)
     //       with the parent being a nspace object (instead of class object)
     Template t_register = getTemplate("jsv8_register_static_variable");
     t_register.replace(T_PARENT, Getattr(current_namespace, NAME))
-        .replace(T_NAME, state.variable(NAME))
+        .replace(T_NAME, Swig_name_mangle(state.variable(NAME)))
         .replace(T_GETTER, state.variable(GETTER))
         .replace(T_SETTER, state.variable(SETTER))
         .trim()
